977ca8b14275dbc01c5cf66036259a05d750ca6d
Vinícius Leite <viniciusleitealves@gmail.com>
2024-06-10 20:20:04 -0300

    Atualizando usuario postgres

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:viniarchizx@localhost/treDatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/detectai"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
