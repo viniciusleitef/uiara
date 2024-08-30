89057a9c97bf683df92ddcc88ce8eeb34310ba41
Vinícius Leite <viniciusleitealves@gmail.com>
2024-08-30 16:52:58 -0300

    fix: romullo fixed needed to run

--- fastapi/utils/email.py
+++ fastapi/utils/email.py
@@ -7,6 +7,7 @@ EMAIL_USER = os.getenv('EMAIL_USER')
 EMAIL_PASS = os.getenv('EMAIL_PASS')
 EMAIL_SERV = os.getenv('EMAIL_SERV')
 EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
+EMAIL_FROM = os.getenv('EMAIL_FROM')
 
 def smtp_setup(
         to_email: str, 
@@ -15,7 +16,7 @@ def smtp_setup(
     ) -> None:
 
     msg = MIMEMultipart()
-    msg['From'] = EMAIL_USER
+    msg['From'] = EMAIL_FROM
     msg['To'] = to_email
     msg['Subject'] = subject
     msg.attach(
@@ -26,7 +27,7 @@ def smtp_setup(
     server.starttls()
     server.login(EMAIL_USER, EMAIL_PASS)
     text = msg.as_string()
-    server.sendmail(EMAIL_USER, to_email, text)
+    server.sendmail(EMAIL_FROM, to_email, text)
     server.quit()
 
     return
